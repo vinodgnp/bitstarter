2c99d5cc6b67c9152bb50f5d7495090f42997664
Ubuntu <ubuntu@ip-10-164-84-57.ec2.internal>
2013-07-10 22:30:27 +0000

    Modified web.js to read inext.html-fixed error

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var buffer = new Buffer(fs.readFileSync('index.html');
+var buffer = new Buffer(fs.readFileSync('/index.html'));
+
 app.get('/', function(request, response) {
-  response.send(buffer.toString()
-);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
