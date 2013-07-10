f3f83409b8110ac42e34c2352126898a4df948fc
Ubuntu <ubuntu@ip-10-164-84-57.ec2.internal>
2013-07-10 22:33:19 +0000

    Modified web.js to read inext.html-fixed error loading file

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var buffer = new Buffer(fs.readFileSync('/index.html'));
+var buffer = new Buffer(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
